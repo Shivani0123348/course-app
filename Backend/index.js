d3d92083c93a95dc0de803161cd202d13d2fcbcd
Shivani0123348 <shivanibajpai542@gmail.com>
2025-12-23 22:14:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,14 +14,17 @@ dotenv.config();
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-      origin: process.env.FRONTEND_URL,
+app.use(
+  cors({
+    origin: "https://course-app-five-gold.vercel.app", // your exact Vercel URL
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],  
-    }
-))
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Allow all preflight OPTIONS
+app.options("*", cors());
 app.use(
     fileUpload
         ({
